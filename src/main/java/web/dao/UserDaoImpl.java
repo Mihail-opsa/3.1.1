a787f5f6b3a425b56dc180fee13b5f24470cc33a
Mihail-opsa <misha7789112@gmail.com>
2025-11-11 00:21:46 +0300

    spring boot

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -3,9 +3,9 @@ package web.dao;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
